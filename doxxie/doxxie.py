a2e702154aacfb05f360cbca19301b5ea8d92696
kyle v <kyle@verhoog.ca>
2021-03-27 23:17:36 -0400

    Add docstrings, tests

--- doxxie/doxxie.py
+++ doxxie/doxxie.py
@@ -14,8 +14,10 @@ from mypy.nodes import FuncDef
 from mypy.nodes import SymbolTableNode
 from mypy.nodes import TypeInfo
 from mypy.nodes import Var
+from mypy.options import Options
 from mypy.plugin import Plugin
 from mypy.types import CallableType
+from mypy.types import NoneType  # noqa used doctest
 from mypy.types import Type as MypyType
 from mypy.types import UnionType
 
@@ -24,33 +26,52 @@ log = logging.getLogger(__name__)
 
 
 class MypyPlugin(Plugin):
-    def __init__(self, opts):
+    def __init__(
+        self, opts: Options, includes="", excludes="", out=".public_api", debug=False
+    ):
         super().__init__(opts)
-        if os.environ.get("DOXXIE_DEBUG", False):
+        if os.environ.get("DOXXIE_DEBUG", debug):
             logging.basicConfig(level=logging.DEBUG)
 
-        self._api_hints: Set[str] = set()
-
-        includes = os.environ.get("DOXXIE_INCLUDES", "")
+        includes = os.environ.get("DOXXIE_INCLUDES", includes)
         self._includes: List[str] = includes.split(",") if includes else []
 
-        excludes = os.environ.get("DOXXIE_EXCLUDES", "")
+        excludes = os.environ.get("DOXXIE_EXCLUDES", excludes)
         self._excludes: List[str] = excludes.split(",") if excludes else []
-        self._out_file = os.environ.get("DOXXIE_OUTFILE", ".public_api")
+        self._out_file = os.environ.get("DOXXIE_OUTFILE", out)
         log.debug(
             "doxxie initialized with includes=%r, excludes=%r, outfile=%r",
             self._includes,
             self._excludes,
             self._out_file,
         )
+
+        self._api_hints: Set[str] = set()
         atexit.register(self._done)
 
     def _in_includes(self, name: str) -> bool:
+        """
+        >>> MypyPlugin(Options())._in_includes("mod.fn")
+        False
+        >>> MypyPlugin(Options(), includes="mod")._in_includes("mod.fn")
+        True
+        >>> MypyPlugin(Options(), includes="mod")._in_includes("mod._internal")
+        True
+        """
         return any(name.startswith(i) for i in self._includes)
 
     def set_modules(self, modules):
+        """Set the plugin modules.
+        This method is called by mypy when new modules are processed. mypy
+        typically caches the modules that it processes which means that
+        this method will only be called with uncached modules. Hence it's
+        recommended that the plugin only be used with the --no-incremental
+        option of mypy.
+        """
         log.debug("got modules %r", modules.keys())
-        # At this point class attributes and methods are not yet evaluated.
+        # At this point class attribute and method types are not yet evaluated
+        # so _api_hints will only contain classes, functions and top-level
+        # objects.
         for modname, mod in modules.items():
             if not self._in_includes(modname):
                 continue
@@ -66,6 +87,16 @@ class MypyPlugin(Plugin):
 
     @staticmethod
     def _is_private_cls(fullname: str) -> bool:
+        """
+        >>> MypyPlugin._is_private_cls("mod.Class")
+        False
+        >>> MypyPlugin._is_private_cls("mod._Class")
+        True
+        >>> MypyPlugin._is_private_cls("_Class")
+        True
+        >>> MypyPlugin._is_private_cls("Class")
+        False
+        """
         split = fullname.split(".")
         attr = split[-1]
         if attr.startswith("_"):
@@ -74,6 +105,22 @@ class MypyPlugin(Plugin):
 
     @staticmethod
     def _is_private_fn(fullname: str) -> bool:
+        """
+        >>> MypyPlugin._is_private_fn("_fn")
+        True
+        >>> MypyPlugin._is_private_fn("fn")
+        False
+        >>> MypyPlugin._is_private_fn("x.y._fn")
+        True
+        >>> MypyPlugin._is_private_fn("x._fn")
+        True
+        >>> MypyPlugin._is_private_fn("x.y.fn")
+        False
+        >>> MypyPlugin._is_private_fn("x.z")
+        False
+        >>> MypyPlugin._is_private_fn("mod.__init__")
+        True
+        """
         split = fullname.split(".")
         attr = split[-1]
         if attr.startswith("_"):
@@ -83,13 +130,15 @@ class MypyPlugin(Plugin):
     @staticmethod
     def _is_private_attr(attr: str) -> bool:
         """
-        >>> MypyPlugin._is_private_attr("x.y._z")
+        >>> MypyPlugin._is_private_attr("mod.cls._z")
         True
-        >>> MypyPlugin._is_private_attr("x._z")
+        >>> MypyPlugin._is_private_attr("cls._z")
         True
-        >>> MypyPlugin._is_private_attr("x.y.z")
+        >>> MypyPlugin._is_private_attr("mod._Cls.z")
         False
         >>> MypyPlugin._is_private_attr("x.z")
+        False
+        >>> MypyPlugin._is_private_attr("X.__init__")
         False
         """
         split = attr.split(".")
@@ -99,13 +148,41 @@ class MypyPlugin(Plugin):
             return True
         return False
 
+    def _in_excluded(self, fullname: str) -> bool:
+        """
+        >>> MypyPlugin(Options())._in_excluded("a.b.c")
+        False
+        >>> MypyPlugin(Options(), excludes="mod.internal")._in_excluded("mod")
+        False
+        >>> MypyPlugin(Options(), excludes="mod.internal")._in_excluded("mod.internal")
+        True
+        >>> MypyPlugin(Options(), excludes="mod.internal")._in_excluded("mod.internal.x")
+        True
+        """
+        return any(fullname.startswith(e) for e in self._excludes)
+
+    @staticmethod
+    def _get_types(typ: MypyType) -> Sequence[MypyType]:
+        """Get all possible types that are used in a type.
+        >>> MypyPlugin._get_types(UnionType(items=[NoneType(), NoneType()]))
+        [None, None]
+        """
+        # TODO: handle list, dict, etc - way to handle arbitrary?
+        if isinstance(typ, UnionType):
+            return typ.items
+        return [typ]
+
     def _initial_public_api(self) -> Dict[str, SymbolTableNode]:
+        """Generate the initial public API.
+
+        The initial public API is all public objects that are exposed in public
+        modules. Public objects are, by PEP-8 standards, defined to be objects
+        whose names start with an underscore character.
+
+        The initial API is built off of the hints collected from set_modules.
+        """
         # Filter out any excluded modules.
-        hints = [
-            h
-            for h in self._api_hints
-            if not any(h.startswith(i) for i in self._excludes)
-        ]
+        hints = [h for h in self._api_hints if not self._in_excluded(h)]
         api = {}
         for name in hints:
             node = self.lookup_fully_qualified(name)
@@ -129,13 +206,6 @@ class MypyPlugin(Plugin):
             else:
                 raise NotImplementedError
         return api
-
-    @staticmethod
-    def _get_types(typ: MypyType) -> Sequence[MypyType]:
-        # TODO: handle list, dict, etc - way to handle arbitrary?
-        if isinstance(typ, UnionType):
-            return typ.items
-        return [typ]
 
     def _expand_api(
         self, api: Dict[str, SymbolTableNode]
@@ -178,7 +248,7 @@ class MypyPlugin(Plugin):
                     to_add.append(self.lookup_fully_qualified(stype))
             else:
                 # TODO: anything to handle here?
-                pass
+                log.debug("%r not supported", node.node)
         return public_api
 
     def _done(self):
@@ -187,7 +257,8 @@ class MypyPlugin(Plugin):
         public_api = self._expand_api(initial_api)
 
         # Add types to public API.
-        typed_public_api = {k: str(v) if v.node else v for k, v in public_api.items()}
+        typed_public_api = {k: str(v) for k, v in public_api.items()}
+        log.debug("typed public api %r", typed_public_api)
         with open(self._out_file, "w") as f:
             pprint.pprint(typed_public_api, stream=f, width=250)
         return
